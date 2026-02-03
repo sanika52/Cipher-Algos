9fcada174a98eed209ad9eb80e76bbe18817d153
GauriDipakBansode <gauridipakbansode@gmail.com>
2026-02-03 15:08:40 +0530

    Code updated

--- Caesar_Cipher.java
+++ Caesar_Cipher.java
@@ -1,8 +1,6 @@
-import java.util.Scanner;
-
 class CaesarCipher {
 
-    // Encrypts text using a shift of s
+    // Encryption function
     public static String encrypt(String text, int s) {
         StringBuffer result = new StringBuffer();
 
@@ -10,44 +8,20 @@ class CaesarCipher {
             char ch = text.charAt(i);
 
             if (Character.isUpperCase(ch)) {
-                char encryptedChar = (char) ((ch + s - 65) % 26 + 65);
-                result.append(encryptedChar);
+                result.append((char) ((ch + s - 65) % 26 + 65));
             } 
             else if (Character.isLowerCase(ch)) {
-                char encryptedChar = (char) ((ch + s - 97) % 26 + 97);
-                result.append(encryptedChar);
+                result.append((char) ((ch + s - 97) % 26 + 97));
             } 
             else {
-                // For spaces or special characters
-                result.append(ch);
+                result.append(ch); // special characters
             }
         }
         return result.toString();
     }
 
-    // Decrypts text using a shift of s
+    // Decryption function
     public static String decrypt(String cipherText, int s) {
         return encrypt(cipherText, 26 - s);
-    }
-
-    // Driver code
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the text: ");
-        String text = sc.nextLine();
-
-        System.out.print("Enter the shift value: ");
-        int s = sc.nextInt();
-
-        String encryptedText = encrypt(text, s);
-        String decryptedText = decrypt(encryptedText, s);
-
-        System.out.println("\nOriginal Text  : " + text);
-        System.out.println("Encrypted Text : " + encryptedText);
-        System.out.println("Decrypted Text : " + decryptedText);
-
-        sc.close();
     }
 }
