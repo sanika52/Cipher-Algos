0cd6810a73729fe75a6834042816fa575bf517ec
GauriDipakBansode <gauridipakbansode@gmail.com>
2026-02-03 14:55:35 +0530

    caesar cipher is added

--- Caesar_Cipher.java
+++ Caesar_Cipher.java
@@ -1,36 +1,53 @@
-//A Java Program to illustrate Caesar Cipher Technique
-class CaesarCipher
-{
-    // Encrypts text using a shift of s
-    public static StringBuffer encrypt(String text, int s)
-    {
-        StringBuffer result= new StringBuffer();
+import java.util.Scanner;
 
-        for (int i=0; i<text.length(); i++)
-        {
-            if (Character.isUpperCase(text.charAt(i)))
-            {
-                char ch = (char)(((int)text.charAt(i) +
-                                  s - 65) % 26 + 65);
-                result.append(ch);
-            }
-            else
-            {
-                char ch = (char)(((int)text.charAt(i) +
-                                  s - 97) % 26 + 97);
+class CaesarCipher {
+
+    // Encrypts text using a shift of s
+    public static String encrypt(String text, int s) {
+        StringBuffer result = new StringBuffer();
+
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+
+            if (Character.isUpperCase(ch)) {
+                char encryptedChar = (char) ((ch + s - 65) % 26 + 65);
+                result.append(encryptedChar);
+            } 
+            else if (Character.isLowerCase(ch)) {
+                char encryptedChar = (char) ((ch + s - 97) % 26 + 97);
+                result.append(encryptedChar);
+            } 
+            else {
+                // For spaces or special characters
                 result.append(ch);
             }
         }
-        return result;
+        return result.toString();
+    }
+
+    // Decrypts text using a shift of s
+    public static String decrypt(String cipherText, int s) {
+        return encrypt(cipherText, 26 - s);
     }
 
     // Driver code
-    public static void main(String[] args)
-    {
-        String text = "ATTACKATONCE";
-        int s = 4;
-        System.out.println("Text  : " + text);
-        System.out.println("Shift : " + s);
-        System.out.println("Cipher: " + encrypt(text, s));
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the text: ");
+        String text = sc.nextLine();
+
+        System.out.print("Enter the shift value: ");
+        int s = sc.nextInt();
+
+        String encryptedText = encrypt(text, s);
+        String decryptedText = decrypt(encryptedText, s);
+
+        System.out.println("\nOriginal Text  : " + text);
+        System.out.println("Encrypted Text : " + encryptedText);
+        System.out.println("Decrypted Text : " + decryptedText);
+
+        sc.close();
     }
 }
